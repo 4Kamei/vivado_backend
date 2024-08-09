a97b558b9617d9d5cc107c00627096df651d71ce
Aleksander Kaminski <aleksander.kaminski@protonmail.com>
2024-08-09 15:48:26 +0100

    Updated environment

--- shell.nix
+++ shell.nix
@@ -1,7 +1,23 @@
 { pkgs ? import <nixpkgs> {} }:
-pkgs.mkShell {
+let
+    pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/cf8cc1201be8bc71b7cbbbdaf349b22f4f99c7ae.tar.gz") {};
+in pkgs.mkShell {
 
-  nativeBuildInputs = with pkgs.buildPackages; [
-    scrot	
+  nativeBuildInputs = with pkgs.buildPackages; [	
+    zlib
+
+    gtkwave #for viewing vcd/fst
+
+    verilog #icarus verilog
+    verilator
+
+    #python
+    python311
+    python311Packages.cocotb
+    python311Packages.pyzmq
+    python311Packages.pytest
+
+    cmake
   ];
+
 }
