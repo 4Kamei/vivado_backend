c414bf8852341f9507bdcbd0bb84014dba8de996
Aleksander Kaminski <aleksander.kaminski@protonmail.com>
2024-08-11 08:44:09 +0100

    Added negedge resets, updated tests for rx

--- design/sources/uart_rx.sv
+++ design/sources/uart_rx.sv
@@ -6,81 +6,91 @@ module uart_rx
          parameter BAUD_RATE = 12_000,
          parameter PARITY_BIT = 0
      )(
-        input wire i_uart_clk,
-        input wire i_uart_in,
-        input wire i_reset,
+        input wire i_clk,
+        input wire i_uart_rx,
+        input wire i_rst_n,
 
-        output reg [7:0] o_data,
-        output reg o_data_out_strobe
+        output reg [7:0] o_rx_data,
+        output reg o_rx_en
     );
     
     localparam CLOCKS_PER_BAUD = CLOCK_FREQUENCY/BAUD_RATE;
 
     localparam DELAY_WIDTH = $clog2(CLOCK_FREQUENCY * 3/2);
 
-    reg receive_state = 0;
-    reg [3:0] byte_counter = 0;
-    reg [7:0] data_reg = 0;
+    reg receive_state;
+    reg [3:0] byte_counter;
+    reg [7:0] data_reg;
     reg uart_in_sync, uart_in_sync1;
     
-    always_ff @(posedge i_uart_clk) begin : proc_uart_in_sync
+    always_ff @(posedge i_clk) begin : proc_uart_in_sync
         uart_in_sync <= uart_in_sync1;
-        uart_in_sync1 <= i_uart_in;
+        uart_in_sync <= i_uart_rx;
     end
     
     //detects 1-0 edge 
-    reg past_value = 0;
-    reg edge_strobe = 0;
-    reg has_bits = 0;
+    reg past_value;
+    reg edge_strobe;
+    reg has_bits;
 
-    always_ff @(posedge i_uart_clk) begin : proc_edge_detector
-        edge_strobe = {past_value, uart_in_sync} == 2'b10;
-        past_value <= uart_in_sync;
-    end
+    always_comb edge_strobe = {past_value, uart_in_sync} == 2'b10;
     
-    always_ff @(posedge i_uart_clk) begin : proc_receive_state
-        o_data_out_strobe <= 0;
-
-        if (edge_strobe & !receive_state) begin
-            receive_state <= 1;
-            has_bits <= 1;
-            uart_clk_div_counter <= (CLOCKS_PER_BAUD + CLOCKS_PER_BAUD/2);
-            byte_counter <= 4'b1111;
+    always_ff @(posedge i_clk or negedge i_rst_n) begin : proc_edge_detector
+        if (!i_rst_n) begin
+            past_value <= 1'b1;
+        end else begin
+            past_value <= uart_in_sync;
         end
-        if (receive_state & byte_counter == 4'b1000) begin
-            if (has_bits) begin
-                o_data <= data_reg;
-                has_bits <= 0;
-            end else begin
-                if (uart_in_sync) begin
-                    receive_state <= 0;                
-                    o_data_out_strobe <= 1;
-                end
+    end
+
+    always_ff @(posedge i_clk or negedge i_rst_n) begin : proc_receive_state
+        if (!i_rst_n) begin
+           has_bits <= 0;
+           receive_state <= 0;
+        end else begin
+            o_rx_en <= 0;
+            if (edge_strobe & !receive_state) begin
+                receive_state <= 1'b1;
+                has_bits <= 1'b1;
+                uart_clk_div_counter <= (CLOCKS_PER_BAUD + CLOCKS_PER_BAUD/2);
+                byte_counter <= 4'b1111;
             end
-            byte_counter <= 4'b1000;
+            if (receive_state & byte_counter == 4'b1000) begin
+                if (has_bits) begin
+                    o_rx_data <= data_reg;
+                    has_bits <= 0;
+                    receive_state <= 0;                
+                    o_rx_en <= 1'b1;
+                end
+                byte_counter <= 4'b1000;
+            end
         end
     end
-    
 
     //strobes every time we should read a bit
     reg uart_clk_strobe;
 
     reg [DELAY_WIDTH:0] uart_clk_div_counter;
-
-    always_ff @(posedge i_uart_clk) begin : proc_uart_clk_strobe
-        uart_clk_strobe <= 0;
-        if (receive_state) begin
-            if (uart_clk_div_counter == 0) begin
-                uart_clk_div_counter <= CLOCKS_PER_BAUD - 1;
-                byte_counter <= byte_counter + 1;
-                uart_clk_strobe <= 1;
-            end else begin
-                uart_clk_div_counter <= uart_clk_div_counter - 1;
+    
+    always_ff @(posedge i_clk or negedge i_rst_n) begin : proc_uart_clk_strobe
+        if (!i_rst_n) begin
+            //Reset nothing in this block, but needs to have a reset to
+            //prevent tools from 
+        end else begin
+            uart_clk_strobe <= 0;
+            if (receive_state) begin
+                if (uart_clk_div_counter == 0) begin
+                    uart_clk_div_counter <= CLOCKS_PER_BAUD - 1;
+                    byte_counter <= byte_counter + 1;
+                    uart_clk_strobe <= 1;
+                end else begin
+                    uart_clk_div_counter <= uart_clk_div_counter - 1;
+                end
             end
         end
     end
-    
-    always_ff @(posedge i_uart_clk) begin : proc_data_save
+
+    always_ff @(posedge i_clk) begin : proc_data_save
         if (uart_clk_strobe & has_bits) begin
             data_reg[byte_counter[2:0]] <= uart_in_sync;
         end
--- design/sources/uart_tx.sv
+++ design/sources/uart_tx.sv
@@ -5,64 +5,92 @@ module uart_tx #(
         parameter CLOCK_FREQUENCY = 12_000_000,
         parameter BAUD_RATE = 115200
     ) (
-        input wire i_clk,
-        input wire i_tx_en,
-        input wire [7:0] i_tx_data,
-        input wire i_rst_n,
+        input logic i_clk,
+        input logic i_rst_n,
+        
+        input logic i_tx_en,
+        input logic [7:0] i_tx_data,
 
-        output reg o_uart_tx,
-        output reg o_tx_rd
+        output logic o_uart_tx,
+        output logic o_uart_busy
     );
 
-    localparam CLOCKS_PER_BAUD = CLOCK_FREQUENCY/BAUD_RATE;
+    //TODO can rewrite without the weird indexing into an array, but with
+    //a bit-shift instead.... still need the counter though, but it probably
+    //simplifies the decoding logic a bit
 
-    reg [3:0] byte_counter;
-    wire is_transmitting = 0;//byte_counter ~= 4'b1111;
-    reg [7:0] sending_data;
-    reg [DELAY_WIDTH: 0] uart_clk_counter;
+    localparam CLOCKS_PER_BAUD_ = CLOCK_FREQUENCY/BAUD_RATE;
+    localparam UART_TX_CLK_COUNTER_SIZE = $clog2(CLOCKS_PER_BAUD_);
+    //TODO can probably take a bit off this counter?
+    //but on the other hand, what really is a single bit between friends
+    localparam CLOCKS_PER_BAUD  = CLOCKS_PER_BAUD_[UART_TX_CLK_COUNTER_SIZE:0]; 
 
-    always_ff @(posedge i_clk or negedge i_rst_n) begin : byte_counter_block
-        if (~i_rst_n) begin
-            byte_counter <= 4'b1111;
+    localparam NUM_STOP_BIT = 3;
+    localparam DATA_LEN = 8;
+
+    localparam TRANSMIT_PATTERN_LEN = NUM_STOP_BIT + DATA_LEN + 1;
+
+    logic [11:0] transmit_pattern;
+    always_comb transmit_pattern = {1'b0, {i_tx_data}, {NUM_STOP_BIT{1'b1}}};
+    logic [11:0] transmit_pattern_q;
+    wire  [11:0] transmit_pattern_rev;
+    logic transmitting;
+
+    generate
+        for (genvar n = 0 ; n < TRANSMIT_PATTERN_LEN; n = n + 1) begin
+            always_comb transmit_pattern_rev[n] = transmit_pattern_q[TRANSMIT_PATTERN_LEN - 1 - n]; // Reverse video data buss bit order 
+        end
+    endgenerate
+
+    //Transmit state and setup
+    always @(posedge i_clk or negedge i_rst_n) begin
+        if (!i_rst_n) begin
+            //If reset, we reset the 'output_pattern_index' to 0, which
+            //indexes into the msb of this -> Idle UART state is 1 hence leave
+            //the reset value
+            transmit_pattern_q <= {1'b1, {DATA_LEN{1'b0}}, {NUM_STOP_BIT{1'b1}}};
+            transmitting <= 1'b0;
         end else begin
-            if (~is_transmitting) begin
-                if (i_tx_en) begin
-                    sending_data = {0, i_tx_data, 1, 1};
-                    byte_counter = 10;
-                end 
-            end else begin
-                if (uart_clk_strobe) begin
-                    byte_counter <= byte_counter - 1;
-                end
+            if (i_tx_en && !transmitting) begin
+                transmit_pattern_q <= transmit_pattern;
+                transmitting <= 1'b1;
+            end
+            if (output_pattern_index == TRANSMIT_PATTERN_LEN - 1 && uart_tx_clk_enable) begin
+                transmitting <= 1'b0;
+            end
+        end
+    end
+    
+    logic [3:0] output_pattern_index;
+    always @(posedge i_clk or negedge i_rst_n) begin
+        if (!i_rst_n) begin
+           output_pattern_index <= 4'b0000;
+        end else begin
+            if (transmitting && uart_tx_clk_enable && output_pattern_index < TRANSMIT_PATTERN_LEN - 1) begin
+                output_pattern_index <= output_pattern_index + 1'b1;
             end
         end
     end
 
-    always_ff @(posedge i_clk or negedge  i_rst_n) begin : ps_clk_divider
-        uart_clk_strobe <= 0;
-        if (~i_rst_n) begin
-            uart_clk_strobe <= 0;
-            uart_clk_counter <= 0;
-        end else begin
-           if (is_transmitting) begin
-               if (uart_clk_counter == 0) begin
-                   uart_clk_counter <= CLOCKS_PER_BAUD;
-                   uart_clk_strobe <= 1;
-               end else begin
-                   uart_clk_counter <= uart_clk_counter - 1;
-               end
-           end
-       end
-    end
-
-    always_ff @(posedge i_clk) begin : output_assignment
-        if (is_transmitting) begin
-            o_uart_tx <= sending_data[byte_counter];
-        end else begin
-            o_uart_tx <= 1;
+    logic [UART_TX_CLK_COUNTER_SIZE:0] uart_tx_clk_enable_counter;
+    logic uart_tx_clk_enable;
+    //Generate the 'uart clk' that's responsible for
+    always @(posedge i_clk) begin
+        uart_tx_clk_enable <= 0;
+        if (transmitting) begin
+            //TODO count down 'CLOCKS PER BAUD'
+            if (uart_tx_clk_enable_counter == CLOCKS_PER_BAUD) begin
+                uart_tx_clk_enable_counter <= 0;
+                uart_tx_clk_enable <= 1;
+            end else begin
+                uart_tx_clk_enable_counter <= uart_tx_clk_enable_counter + 1'b1;
+            end
+        end else begin      
+            uart_tx_clk_enable_counter <= 0;
         end
     end
-
-    assign o_tx_rd = ~is_transmitting;
+    
+    assign o_uart_tx = transmit_pattern_rev[output_pattern_index];
+    assign o_uart_busy = transmitting;
 
 endmodule // uart_tx
