f84e6f67ced007cc5e41870804748d69d604d058
Aleksander Kaminski <aleksander.kaminski@protonmail.com>
2024-08-23 12:43:49 +0100

    Added cocotbext-axi as dependency

--- shell.nix
+++ shell.nix
@@ -1,10 +1,28 @@
 { pkgs ? import <nixpkgs> {} }:
 let
     pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/cf8cc1201be8bc71b7cbbbdaf349b22f4f99c7ae.tar.gz") {};
+    
+    af-cocotbext-axi = with pkgs.python3Packages; buildPythonPackage rec {
+      name = "cocotbext-axi";
+      version = "master";
+
+      src = pkgs.fetchgit {
+        url = "https://github.com/alexforencich/cocotbext-axi";
+        rev = "v0.1.24";  
+        hash = "sha256-/OBFezpsmWQyCPuYs/3Yan9v9GrQ5wvEw4PU6pLugkI=";
+      };
+
+      propagatedBuildInputs = [ pip cocotb cocotb-bus ];
+    };
+
 in pkgs.mkShell {
+
+  
 
   nativeBuildInputs = with pkgs.buildPackages; [	
     zlib
+    
+    
 
     gtkwave #for viewing vcd/fst
 
@@ -14,9 +32,13 @@ in pkgs.mkShell {
     #python
     python311
     python311Packages.cocotb
+    python311Packages.cocotb-bus
     python311Packages.pyzmq
     python311Packages.pytest
     python311Packages.pyserial
+
+    #
+    af-cocotbext-axi
 
     cmake
   ];
